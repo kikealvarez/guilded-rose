70db848781d7e8ae13e18709d4a97ecc4419936a
enrique-alvarez <enrique.alvarez@distilled.ie>
2018-12-17 14:43:13 +0000

    add unit tests and refactor with strategy pattern

--- java/src/main/java/ie/distilledsch/codekata/model/Item.java
+++ java/src/main/java/ie/distilledsch/codekata/model/Item.java
@@ -1,5 +1,7 @@
 package ie.distilledsch.codekata.model;
 
+import java.util.function.Consumer;
+
 /**
  * Do not change this class for this exercise
  */
@@ -9,14 +11,48 @@ public class Item {
   public int sellIn;
   public int quality;
 
-  public Item(String name, int sellIn, int quality) {
+  public Consumer<Item> behavior;
+
+  public Item(String name, int sellIn, int quality, Consumer<Item> behavior) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+    this.behavior = behavior;
+  }
+
+  public Item(String name, int sellIn, int quality) {
+    this(name, sellIn, quality, item -> {});
   }
 
   @Override
   public String toString() {
     return this.name + ", " + this.sellIn + ", " + this.quality;
+  }
+
+  public void daysPassed() {
+    this.sellIn = this.sellIn - 1;
+    behavior.accept(this);
+  }
+
+  public static Consumer<Item> decreaseDefault() {
+    return item -> {
+      if (item.sellIn == 0) {
+        item.quality -= 2;
+      } else {
+        item.quality -= 1;
+      }
+    };
+  }
+
+  public static Consumer<Item> increaseDefault() {
+    return item -> {
+      if (item.quality <= 50) {
+        item.quality += 1;
+      }
+    };
+  }
+
+  public static Consumer<Item> decreaseDouble() {
+    return item -> item.quality -= 2;
   }
 }
--- java/src/test/java/ie/distilledsch/codekata/GildedRoseTest.java
+++ java/src/test/java/ie/distilledsch/codekata/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package ie.distilledsch.codekata;
 
+
 import ie.distilledsch.codekata.model.Item;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,13 +9,149 @@ public class GildedRoseTest {
 
   @Test
   public void testPlaceholder() {
-    Item[] items = new Item[] { new Item("foo", 0, 0) };
+    Item[] items = new Item[]{new Item("foo", 0, 0)};
 
     GildedRose app = new GildedRose(items);
 
     app.updateQualityOfItemsAfterOneDayHasPassed();
 
-    Assert.assertEquals("fixme", app.items[0].name);
+    Assert.assertEquals("foo", app.items[0].name);
   }
+
+  @Test
+  public void testItemQualityDecreases() {
+    Item[] items = new Item[]{new Item("foo", 5, 5, Item.decreaseDefault())};
+
+
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(4, app.items[0].sellIn);
+    Assert.assertEquals(4, app.items[0].quality);
+  }
+
+  @Test
+  public void testItemQualityNeverNegative() {
+    Item[] items = new Item[]{new Item("foo", 1, 0)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(0, app.items[0].quality);
+  }
+
+  @Test
+  public void testItemQualityDecreasesDoublyAfterSellingDate() {
+    Item[] items = new Item[]{new Item("foo", 1, 4,Item.decreaseDefault())};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(2, app.items[0].quality);
+  }
+
+  @Test
+  public void testBrieIncreasesBy2IfDate10orLessEveryDay() {
+    Item[] items = new Item[]{new Item("Aged Brie", 10, 4,Item.increaseDefault())};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(5, app.items[0].quality);
+  }
+
+  @Test
+  public void testItemDOntIncreaseOver50() {
+    Item[] items = new Item[]{new Item("Aged Brie", 10, 49,Item.increaseDefault())};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(50, app.items[0].quality);
+  }
+
+  @Test
+  public void testSulfuraKeepsQuality() {
+    Item[] items = new Item[]{new Item("Sulfuras", 10, 10)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(10, app.items[0].quality);
+  }
+
+  @Test
+  public void testBpIncreasesBy2IfDate10orLessEveryDay() {
+    Item[] items = new Item[]{new Item("Backstage passes", 10, 4)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(6, app.items[0].quality);
+  }
+
+  @Test
+  public void testBpIncreasesBy3IfDate10orLessEveryDay() {
+    Item[] items = new Item[]{new Item("Backstage passes", 5, 4)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(7, app.items[0].quality);
+  }
+
+  @Test
+  public void testBpDropTo0AfterSellingDate() {
+    Item[] items = new Item[]{new Item("Backstage passes", 0, 4)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(0, app.items[0].quality);
+  }
+
+  @Test
+  public void testBpIncreasesBy1() {
+    Item[] items = new Item[]{new Item("Backstage passes", 11, 4)};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(5, app.items[0].quality);
+  }
+
+  @Test
+  public void testConjuredDecreasesBy2() {
+    Item[] items = new Item[]{new Item("Conjured", 11, 4,Item.decreaseDouble())};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(2, app.items[0].quality);
+  }
+
+  @Test
+  public void testConjuredNeverNegative() {
+    Item[] items = new Item[]{new Item("Conjured", 11, 0,Item.decreaseDouble())};
+
+    GildedRose app = new GildedRose(items);
+
+    app.updateQualityOfItemsAfterOneDayHasPassed();
+
+    Assert.assertEquals(0, app.items[0].quality);
+  }
+
 
 }
